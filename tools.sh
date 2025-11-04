5ff4ddc8e3b71206fd1f2fb174086f9394744a77
RevinderDev <michal0kasprzyk@gmail.com>
2025-11-04 16:20:58 +0100

    feat: add zig and lazygit to mise

--- tools.sh
+++ tools.sh
@@ -41,11 +41,5 @@ curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x8
 sudo rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
 
-# Lazygit
-LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
-curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-tar xf lazygit.tar.gz lazygit
-sudo install lazygit -D -t /usr/local/bin/
-
 # Mise and then install tools from mise/config.toml
 curl https://mise.run | sh
